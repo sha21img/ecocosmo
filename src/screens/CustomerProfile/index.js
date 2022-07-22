70964d42b84a395a2d872bc05b1899a0cf905a0b
sha21img <shashankalw.img@gmail.com>
2022-07-22 18:00:07 +0530

    account

--- src/screens/CustomerProfile/index.js
+++ src/screens/CustomerProfile/index.js
@@ -1,13 +1,29 @@
-
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, ImageBackground, ScrollView, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import LinearGradient from 'react-native-linear-gradient';
+import colors from '../../../assets/Colors';
+import {image} from '../../../assets/images';
+import {__} from '../../../Utils/Translation/translation';
+import {styles} from './style';
 
-function CustomerProfile() {
+const MyAccount = () => {
   return (
-    <View>
-      <Text>CustomerProfile</Text>
-    </View>
+    <>
+      <LinearGradient
+        colors={[colors.mainThemeColor1, colors.mainThemeColor2]}
+        style={{flex: 1}}>
+        <ImageBackground source={image.LoginBackground} style={[styles.head]}>
+          <View style={styles.headContainer}>
+            <Image source={image.drawer} style={{height: 20, width: 22}} />
+            <Text>{__('My Account')}</Text>
+            <TouchableOpacity>
+              <Text>{__('🖊 Edit')}</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
-}
-
-export default CustomerProfile;
+};
+export default MyAccount;
