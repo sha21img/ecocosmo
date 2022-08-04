490ca8a7bbf3f457a6d603fe51667ed398961a03
sha21img <shashankalw.img@gmail.com>
2022-08-04 12:25:07 +0530

    map

--- src/screens/MapHistory/index.js
+++ src/screens/MapHistory/index.js
@@ -18,14 +18,42 @@ import MapView, {
   PROVIDER_GOOGLE,
 } from 'react-native-maps';
 const screen = Dimensions.get('window');
+import {useNetInfo} from '@react-native-community/netinfo';
+import Geolocation from 'react-native-geolocation-service';
 const ASPECT_RATIO = screen.width / screen.height;
 const LATITUDE_DELTA = 0.04;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 function MapHistory(props) {
   const [data, setData] = useState();
+  const [coordinate, setCoordinate] = useState({
+    latitude: 26.9110637,
+    longitude: 75.7376412,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
   useEffect(() => {
     getMapHistory();
   }, []);
+  const getLocations = async () => {
+    Geolocation.getCurrentPosition(position => {
+      console.log('positio', position);
+      setCoordinate({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  };
+  const netInfo = useNetInfo();
+  useEffect(() => {
+    // getLocation();
+    if (netInfo.isConnected) {
+      if (Platform.OS == 'android') {
+        getLocations();
+      } else {
+        // checkPermissionIOS();
+      }
+    }
+  }, [netInfo.isConnected]);
   const getMapHistory = async () => {
     const loginDetail = await Storage.getLoginDetail('login_detail');
     let username = loginDetail.accountName;
@@ -37,7 +65,8 @@ function MapHistory(props) {
       date: '2022-07-01',
     };
     const response = await axiosGetData('mapHistory', data);
-    setData(response.data.EventHistory);
+    const filter = response.data.EventHistory.slice(0, 2);
+    setData(filter);
   };
   return (
     <>
@@ -129,53 +158,37 @@ function MapHistory(props) {
           style={{
             flex: 1,
           }}
-          // zoomEnabled={true}
           trackViewChanges={false}
-          // scrollEnabled={false}
-          // pointerEvents="none"
-          // minZoomLevel={15}
           initialRegion={{
-            latitude: 26.9124,
-            longitude: 75.7873,
+            latitude: coordinate.latitude,
+            longitude: coordinate.longitude,
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA,
           }}
-          // provider={PROVIDER_GOOGLE}
-          // followsUserLocation={true}
-          // showsMyLocationButton={true}
-          // showsUserLocation={true}
-          // onPress={e => {
-          //   setCoordinate(e.nativeEvent.coordinate);
-          // }}
-          // onRegionChangeComplete={region => onRegionalChange(region)}
-          // onRegionChangeComplete={region => setCoordinate(region)}
-          // onRegionChange={region => setCoordinate(region)}
-          // onMapReady={() => setMarginBottom(0)}
         >
-          {/* {data?.map((item, index) => {
+          {data?.map((item, index) => {
+            console.log('this is item of car', item);
             return (
-              <> */}
-          <Marker
-            // ref={markerRef}
-            // key={index.toString()}
-            coordinate={{
-              // latitude: parseFloat(item.lat),
-              // longitude: parseFloat(item.lng),
-              latitude: 26.9124,
-              longitude: 75.7873,
-            }}>
-            <Image
-              resizeMode="contain"
-              source={image.carGreenUp}
-              style={{
-                height: 20,
-                width: 70,
-              }}
-            />
-          </Marker>
-          {/* </>
+              <>
+                <Marker
+                  // ref={markerRef}
+                  key={index.toString()}
+                  coordinate={{
+                    latitude: parseFloat(item.lat),
+                    longitude: parseFloat(item.lng),
+                  }}>
+                  <Image
+                    resizeMode="contain"
+                    source={image.carGreenUp}
+                    style={{
+                      height: 20,
+                      width: 70,
+                    }}
+                  />
+                </Marker>
+              </>
             );
-          })} */}
+          })}
         </MapView>
       </View>
     </>
